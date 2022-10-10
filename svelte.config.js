fcc3962c1eff133a2adb499f024f5a0bf98ed3b6
vibovenkat123 <vibovenkat@gmail.com>
2022-10-10 12:04:56 -0500

    added firebase hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,8 +8,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: 'public',
-			assets: 'public',
+			pages: 'build',
+			assets: 'build',
 			fallback: null,
 			precompress: false
 		})
